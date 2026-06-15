0d1f176dfc03c0d56abdc802237eb8ccb2bca59b
shlok <vaneshlok04@gmail.com>
2026-06-15 22:51:25 +0530

    Add CORS middleware to FastAPI application
    frontend eroor cleared

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from database import SessionLocal, engine
 from models import Base, Vendor
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
